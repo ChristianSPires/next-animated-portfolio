e407e1cef7a219a8d77e67600f6394ad4e886c24
Christian de Souza Pires <christian@caletagaming.com>
2024-07-03 13:18:23 -0300

    Feat: testing Framer Motion framework

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,38 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Homepage = () => {
   return (
     <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* Image Container */}
       <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-        <Image src="/hero.png" alt="Hero image" layout="fill" className="object-contain"/>
+        <Image
+          src="/hero.png"
+          alt="Hero image"
+          layout="fill"
+          className="object-contain"
+        />
       </div>
       {/* Text Container */}
       <div className="h-1/2 flex flex-col lg:h-full lg:w-1/2 gap-8 items-center justify-center">
         {/* TITLE */}
-        <h1 className='text-4xl md:text-6xl font-bold'>Crafting Digital Experiences, Designing Tomorrow.</h1>
+        <h1 className="text-4xl md:text-6xl font-bold">
+          Crafting Digital Experiences, Designing Tomorrow.
+        </h1>
         {/* DESC */}
-        <p className='md:text-xl'>Welcome to my digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my portfolio showcases a diverse collection of projects that reflect my commitment to excellence.</p>
+        <p className="md:text-xl">
+          Welcome to my digital canvas, where innovation and creativity
+          converge. With a keen eye for aesthetics and a mastery of code, my
+          portfolio showcases a diverse collection of projects that reflect my
+          commitment to excellence.
+        </p>
         {/* BUTTONS */}
-        <div className='w-full flex gap-4'>
-          <button className='p-4 rounded-lg ring-1 ring-black bg-black text-white'>View My Work</button>
-          <button>Contact Me</button>
+        <div className="w-full flex gap-4">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            View My Work
+          </button>
+          <button className="p-4 rounded-lg ring-1 ring-black">
+            Contact Me
+          </button>
         </div>
       </div>
     </div>
